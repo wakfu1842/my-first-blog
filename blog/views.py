b0ecc91d531cb4ee730356245e8f18f86103ee7f
hbw <wakfu1842@gmail.com>
2016-03-04 11:11:41 +0800

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Post
+"""from 後面的點號意味著 當前目錄 或 當前的應用程序。 因為 views.py 和 models.py 是在同一目錄中，我們只需要使用 . 和 文件的名稱（無 .py) 。 然後我們導入模型（Post)"""
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
